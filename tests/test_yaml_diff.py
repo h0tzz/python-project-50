ed75de457f82b715eabd07c7681fab2c6b23c868
Ilya Malashenko <ilya.malashenko.developer@gmail.com>
2022-10-13 15:27:21 +0300

    asd

--- tests/test_yaml_diff.py
+++ tests/test_yaml_diff.py
@@ -19,30 +19,3 @@ def test2_simple_plain():
                            'plain')
     assert actual == expected.SIMPLE_PLAIN
 
-
-def test3_simple_json():
-    actual = generate_diff('./tests/fixtures/simple_before.yaml',
-                           './tests/fixtures/simple_after.yaml',
-                           'json')
-    assert actual == expected.SIMPLE_JSON
-
-
-def test4_complex_string():
-    actual = generate_diff('./tests/fixtures/complex_before.yaml',
-                           './tests/fixtures/complex_after.yaml',
-                           'string')
-    assert actual == expected.COMPLEX_STRING
-
-
-def test5_complex_plain():
-    actual = generate_diff('./tests/fixtures/complex_before.yaml',
-                           './tests/fixtures/complex_after.yaml',
-                           'plain')
-    assert actual == expected.COMPLEX_PLAIN
-
-
-def test6_complex_json():
-    actual = generate_diff('./tests/fixtures/complex_before.yaml',
-                           './tests/fixtures/complex_after.yaml',
-                           'json')
-    assert actual == expected.COMPLEX_JSON
