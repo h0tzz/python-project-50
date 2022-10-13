9c2769efe519f9c98d65a7c3095728fe17aa5457
Ilya Malashenko <ilya.malashenko.developer@gmail.com>
2022-10-13 10:57:53 +0300

    add test

--- gendiff/input_parser.py
+++ gendiff/input_parser.py
@@ -10,5 +10,6 @@ def parser(file_data, file_type):
     mapping = {
         '.json': json.loads,
         '.yml': lambda file_data: yaml.load(file_data, Loader=yaml.SafeLoader),
+        '.yaml': lambda file_data: yaml.load(file_data, Loader=yaml.SafeLoader),
     }
     return mapping[file_type](file_data)
