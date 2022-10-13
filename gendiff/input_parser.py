c3bdca808ff452d6ce0b6976b129f80078e88c1d
Ilya Malashenko <ilya.malashenko.developer@gmail.com>
2022-10-13 10:44:42 +0300

    fix

--- gendiff/input_parser.py
+++ gendiff/input_parser.py
@@ -9,6 +9,6 @@ def parser(file_data, file_type):
     """Parse input data into appropriate format."""
     mapping = {
         '.json': json.loads,
-        '.yaml': lambda file_data: yaml.load(file_data, Loader=yaml.SafeLoader),
+        '.yml': lambda file_data: yaml.load(file_data, Loader=yaml.SafeLoader),
     }
     return mapping[file_type](file_data)
